356280b3b27e5d6084f249f1093659f56e6bd0ff
troyft5 <troy@fintech5group.com>
2026-06-30 15:20:07 -0400

    fix(favicon): use inline SVG path instead of img data URI to fix server build

--- app/icon.tsx
+++ app/icon.tsx
@@ -6,26 +6,13 @@ export const contentType = 'image/png'
 export default function Icon() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          width: 32,
-          height: 32,
-          background: '#0f1a0f',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: 7,
-        }}
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          width="30"
-          height="30"
-          src={`data:image/svg+xml,${encodeURIComponent(
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -274 2048 2048"><path d="M 24.566742 1394 L 2022.228149 1156.592163 L 1692.278198 107 L 1689.545044 943.003296 Z" fill="#4e9000"/></svg>'
-          )}`}
-          alt=""
-        />
+      <div style={{ width: 32, height: 32, background: '#0f1a0f', display: 'flex', borderRadius: 7 }}>
+        <svg width="32" height="32" viewBox="0 -274 2048 2048">
+          <path
+            d="M 24.566742 1394 L 2022.228149 1156.592163 L 1692.278198 107 L 1689.545044 943.003296 Z"
+            fill="#4e9000"
+          />
+        </svg>
       </div>
     ),
     { ...size }
